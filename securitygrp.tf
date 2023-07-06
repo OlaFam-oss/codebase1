afd1971ca899066638b08be899b8f0c06fb5def1
OlaFam-oss <132849173+OlaFam-oss@users.noreply.github.com>
2023-07-06 15:08:06 +0100

    updated vpc in codebase

--- securitygrp.tf
+++ securitygrp.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2" # Replace with your desired AWS region
+  region = "eu-west-1" # Replace with your desired AWS region
 }
 
 resource "aws_vpc" "example" {
@@ -7,13 +7,13 @@ resource "aws_vpc" "example" {
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.example.id
+  vpc_id            = "vpc-0005bae46051f5692"
   cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-west-2a" # Replace with your desired availability zone
+  availability_zone = "eu-west-1a" # Replace with your desired availability zone
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.example.id
+  vpc_id            = "vpc-0005bae46051f5692"
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2b" # Replace with your desired availability zone
+  availability_zone = "eu-west-1b" # Replace with your desired availability zone
 }
